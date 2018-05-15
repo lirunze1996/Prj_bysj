f4ece90169680ab487244386b95dba2b47466066
李润泽 <runze.li@mathartsys.com>
2018-05-15 18:23:46 +0800

    update:完成数据展示

--- web/webservice/models.py
+++ web/webservice/models.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class movie(models.Model):
+    mid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    count = models.IntegerField()
+    score = models.FloatField()
+
+    class Meta:
+        db_table = "movie"
--- web/webservice/views.py
+++ web/webservice/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from webservice.models import *
 
 
 # Create your views here.
@@ -11,4 +12,14 @@ def index(request):
 
 
 def detail(request):
-    return render(request, 'detail.html')
+    mid = request.GET.get("mid")
+    content = {}
+    if mid is not None:
+        m = movie.objects.filter(mid=mid)
+        content["m"] = m[0]
+        print m
+    movies = movie.objects.all()
+    content["movies"] = movies
+    return render(request, 'detail.html', content)
+
+
